e6f66a3a1fa04ad9f99774d26a70c41a5c032c7b
Michal Szekalski <szekalski.michal@gmail.com>
2023-03-21 22:10:55 +0100

    dodana możliwość usuwania opisu szkolenia

--- src/main/java/pl/javastart/bootcamp/domain/training/description/TrainingDescriptionService.java
+++ src/main/java/pl/javastart/bootcamp/domain/training/description/TrainingDescriptionService.java
@@ -39,4 +39,8 @@ public class TrainingDescriptionService {
         }
         return trainingDescriptionRepository.save(trainingDescription);
     }
+
+    public void deleteById(Long id) {
+        trainingDescriptionRepository.deleteById(id);
+    }
 }
